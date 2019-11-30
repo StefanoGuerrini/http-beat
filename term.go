3d1ac1ec6ea1bc90fd0d5635c94ed4be5b1fceba
Stefano Guerrini <guerrinistefano3@gmail.com>
2019-11-30 13:47:26 +0100

    add term.go file

---
+++ term.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	tm "github.com/buger/goterm"
+	"strconv"
+	"time"
+)
+
+func Monitor() {
+	tm.Clear()
+
+	for {
+		tm.MoveCursor(1, 1)
+		tm.Println("Last beat:", time.Now().Format(time.RFC1123))
+
+		res := Check()
+
+		x := 1
+		y := 3
+
+		tm.MoveCursor(x, y)
+
+		for key, val := range res {
+			tm.Printf(key)
+			tm.MoveCursor(35, y)
+
+			color := tm.GREEN
+			if val >= 500 && val < 599 {
+				color = tm.RED
+			} else if val >= 300 && val < 500 {
+				color = tm.YELLOW
+			}
+
+			tm.Println(tm.Color(strconv.Itoa(val), color))
+			y += 1
+			tm.MoveCursor(x, y)
+		}
+
+		tm.Flush()
+		tm.Clear()
+
+		time.Sleep(time.Duration(GetBeatSeconds()) * time.Second)
+	}
+}
